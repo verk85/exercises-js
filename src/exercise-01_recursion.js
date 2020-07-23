0b75b1ec709f5e878bdd6e342782435255e1b972
VerkGames <verk@VerkGamess-MacBook-Pro.local>
2020-07-23 15:30:42 -0700

    exercise-01 with async/await

--- src/exercise-01_recursion.js
+++ src/exercise-01_recursion.js
@@ -2,13 +2,13 @@
 export const taskFactorySample = (delay, resolve, val) => () =>
   new Promise((res, rej) => setTimeout(resolve ? res : rej, delay, val));
 export const tasks = [
-  taskFactorySample(7000, true, 1),
+  taskFactorySample(1000, true, 1),
   taskFactorySample(2000, true, 2),
   taskFactorySample(3000, false, "error"),
   taskFactorySample(4000, true, 4),
   taskFactorySample(5000, false, "error"),
   taskFactorySample(6000, false, "error"),
-  taskFactorySample(1000, true, 5),
+  taskFactorySample(7000, true, 5),
 ];
 // only run two promises at a time
 export const batch_size = 2;
@@ -19,12 +19,17 @@ export const batch_size = 2;
  *  [{value: 1}, {value:2}, {error: 'error'}, ...]
  */
 
-export async function runBatches(tasks, batch_size) {
-  const tasksDone = [];
+export function runBatches(tasks, batch_size) {
+  let index = 0;
+  const tasksResolved = [];
   const tasksExecuting = [];
 
-  for (let task of tasks) {
-    const p = task()
+  const enqueue = function () {
+    if (index === tasks.length) {
+      return Promise.resolve();
+    }
+
+    const p = Promise.resolve(tasks[index++]())
       .then((value) => {
         return { value };
       })
@@ -32,15 +37,18 @@ export async function runBatches(tasks, batch_size) {
         return { error };
       });
 
-    tasksDone.push(p);
+    tasksResolved.push(p);
 
     const e = p.then(() => tasksExecuting.splice(tasksExecuting.indexOf(e), 1));
     tasksExecuting.push(e);
 
+    let r = Promise.resolve();
+
     if (tasksExecuting.length >= batch_size) {
-      await Promise.race(tasksExecuting);
+      r = Promise.race(tasksExecuting);
     }
-  }
-  return Promise.all(tasksDone);
+    return r.then(() => enqueue());
+  };
+  return enqueue().then(() => Promise.all(tasksResolved));
 }
 //runBatches(tasks, batch_size).then(console.log);
